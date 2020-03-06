c59136dcdd5ed1ae85c6727d9d7a52c4fd5217ef
isbobby <bobbyclex8@gmail.com>
2020-03-07 02:27:44 +0800

    fixed naming

---
+++ src/components/blogic/mediamapping.js
@@ -0,0 +1,232 @@
+import ABB from "./resource/images/ABB.png"
+import AS from "./resource/images/AS.png"
+import APAN from "./resource/images/APAN.png"
+import B1 from "./resource/images/B1.png"
+import BL from "./resource/images/BL.png"
+import DH from "./resource/images/DH.png"
+import DHS from "./resource/images/DHS.png"
+import DS from "./resource/images/DS.png"
+import DSS from "./resource/images/DSS.png"
+import FH from "./resource/images/FH.png"
+import FHS from "./resource/images/FHS.png"
+import GGS from "./resource/images/GGS.png"
+import GYM from "./resource/images/GYM.png"
+import HO from "./resource/images/HO.png"
+import L1C from "./resource/images/L1C.png"
+import L1S from "./resource/images/L1S.png"
+import MPC from "./resource/images/MPC.png"
+import RR from "./resource/images/RR.png"
+import RRS from "./resource/images/RRS.png"
+
+import ABBAS from "./resource/gif/ABBAS.gif"
+import ABBB1 from "./resource/gif/ABBB1.gif"
+import ABBBL from "./resource/gif/ABBBL.gif"
+import APANAS from "./resource/gif/APANAS.gif"
+import ASABB from "./resource/gif/ASABB.gif"
+
+import ASAPAN from "./resource/gif/ASAPAN.gif"
+import ASMPC from "./resource/gif/ASMPC.gif"
+import B1ABB from "./resource/gif/B1ABB.gif"
+import B1GGS from "./resource/gif/B1GGS.gif"
+import B1L1C from "./resource/gif/B1L1C.gif"
+
+import BLABB from "./resource/gif/BLABB.gif"
+import DHDHS from "./resource/gif/DHDHS.gif"
+import DHSDH from "./resource/gif/DHSDH.gif"
+import DHSFHS from "./resource/gif/DHSFHS.gif"
+import DHSRRS from "./resource/gif/DHSRRS.gif"
+
+import DSDSS from "./resource/gif/DSDSS.gif"
+import DSSDS from "./resource/gif/DSSDS.gif"
+import DSSGYM from "./resource/gif/DSSGYM.gif"
+import DSSL1S from "./resource/gif/DSSL1S.gif"
+import FHFHS from "./resource/gif/FHFHS.gif"
+
+import FHSDHS from "./resource/gif/FHSDHS.gif"
+import FHSFH from "./resource/gif/FHSFH.gif"
+import FHSL1S from "./resource/gif/FHSL1S.gif"
+import GGSB1 from "./resource/gif/GGSB1.gif"
+
+import GGSHO from "./resource/gif/GGSHO.gif"
+import GYMDSS from "./resource/gif/GYMDSS.gif"
+import HOGGS from "./resource/gif/HOGGS.gif"
+import HORRS from "./resource/gif/HORRS.gif"
+import L1CB1 from "./resource/gif/L1CB1.gif"
+
+import L1CL1S from "./resource/gif/L1CL1S.gif"
+import L1SDSS from "./resource/gif/L1SDSS.gif"
+import L1SFHS from "./resource/gif/L1SFHS.gif"
+import L1SL1C from "./resource/gif/L1SL1C.gif"
+import MPCAS from "./resource/gif/MPCAS.gif"
+
+import RRRRS from "./resource/gif/RRRRS.gif"
+import RRSDHS from "./resource/gif/RRSDHS.gif"
+import RRSHO from "./resource/gif/RRSHO.gif"
+import RRSRR from "./resource/gif/RRSRR.gif"
+
+
+
+export default {
+  images: {
+    AS:AS,
+    ABB:ABB,
+    APAN:APAN,
+    B1:B1,
+    BL:BL,
+    DH:DH,
+    DHS:DHS,
+    DS:DS,
+    DSS:DSS,
+    FH:FH,
+    FHS:FHS,
+    GGS:GGS,
+    GYM:GYM,
+    HO:HO,
+    L1C:L1C,
+    L1S:L1S,
+    MPC:MPC,
+    RR:RR,
+    RRS:RRS
+  },
+
+  mapList: [
+    {
+      Image: HO,
+      Name: "Hall Office",
+      Key: "HO"
+    },
+    {
+      Image: GGS,
+      Name: "Golden Gate Stairs",
+      Key: "GGS"
+    },
+    {
+      Image: BL,
+      Name: "B Lounge",
+      Key: "BL"
+    },
+    {
+      Image: DH,
+      Name: "DH",
+      Key: "DH"
+    },
+    {
+      Image: DS,
+      Name: "Dance Studio",
+      Key: "DS"
+    },
+    {
+      Image: FH,
+      Name: "Function Hall",
+      Key: "FH"
+    },
+    {
+      Image: GYM,
+      Name: "Gym",
+      Key: "GYM"
+    },
+    {
+      Image: MPC,
+      Name: "MPC",
+      Key: "MPC"
+    },
+    {
+      Image: ABB,
+      Name: "A/B Bridge",
+      Key: "ABB"
+    },
+    {
+      Image: B1,
+      Name: "Main Bridge",
+      Key: "B1"
+    }
+  ],
+
+  eventLists: {
+    MPC: [
+      {
+        Image:
+          "https://lh3.googleusercontent.com/5lsjkpS_tnVC5VHWxJ8jxHv6EBASarC2ufCg1JeROk0gfagqX1IRmaIAJiCaTXk2_bGwvcjHQUK4oeSZoBrZKGq8aeWLdcM7Bheitoezp2EujFv4LLq2qV1MVmJztxExRSVL0Xju9ers4G4eBkvoOG0Bn9SceOqTwvzDmQeN66XTKPekfBO2plZloWijRS15C6J9qeo7-7y356WLxfNbxQT-WTMRa9vTPkCBXDvSH4UaRc7Nztu0GsLHA7mn8Mzwh4SDCkbaZmrjB9KCCcsYqdDcvjLBpRGzlIHZ234R2pxKPAQWv3raNxVT_QvSaEztCk1XrdijBgnbRZS96SDiUddO9AlcMN6dN_NY5goGb3tdYcPoDD2zanBDW_QhwTvbztteGB3MNJATUFcnoaM3bagjsZzN1Wh4m91Ff9yrgyzr543q3KxENN9qS2mTJk3nFMzc68Kc9Pa-TCQA2za3U10Sm1rQN4MwchFq8lcr0JmS6Qy35oR7drq9R79WKc-Z2OcGNEzHErmWNoJzaxY9GvaAVOcI4kWqRLsQ2hScndKv8JrnvXLjZsgRWLTUOya3kHp-xCvT3qcGc8nwDfpL85YfzBm7eRhKuBNRAAtlzKvkTb8oUQlyytUt7XBlKsySL3M5p1d2d19FMctg5a4AMgIxhJntmKapLfiNCxVR9mJIUcpJyRHu5bc=w1948-h1298-no",
+        Caption: "IBG"
+      }
+    ],
+    DH: [
+      {
+        Image:
+          "https://lh3.googleusercontent.com/0DthVI2ihCkksoTHWA8y0d5xDqhtAbDTO76yjm5rIyMK7BMu2EFkIJCSrPFCmXq77TYl-SH9MGf0OIKEgsuiVM_vtWdtwG7eIUZH0k-Z6vzrdcx8AxTQ1Ed5L3nSLDYkRnsyeaGsu7jHAsmzNdzXcVQldcpjKhV1cspZZ9JcV4o9ekEzClIlFYic76wSNFrmfXzZpCJ51m4kB0IKIIt3p_Fbztcarh71-wm28aDh09QOniHFgMibmhS-h9GJ-pChO1yVpk3X3T9zS7fgkKFghHykkcU0LZpWqP2E1pZsa7em8GbjCDUCizQjkFKwYk65SDflK1fcYGaQPvPwk1DMjPEiJ1n2iiXZHg1UuiMZHJroPoO-aAnuPZM7F5C8YzjvBRUtu82RorzwUBoHaBFW7gEg0J_GMkSSeEYwH_uGs6-3hjYXMiOFjfiR6YYk2LRF3nALEgtHVJ8XdgZkpdL7MOXeVD61RMIX-AruZfqCY-ZGKG2m7OJEYuVYB2dPTpSgiTWd7YBXMCBDpIXLgCXtchcqqHM-mW1TRIvNZA_VDEXcWtCZGFEbYMNeDGIiA4pWyE7NBSuHx_50DBu3c4DoYLZNlLMNgTLeQEgX44FJZUaoYLVtFqNdufVpZQhl68f1b9_NdZZvxfXLkYiyTw2Unl75NTW_TKf_A50aqyXWKPJqjoZq8gTsBDM=w1948-h1298-no",
+        Caption: "Halloween"
+      },
+      {
+        Image:
+          "https://lh3.googleusercontent.com/oCVD-J1Xu8TXnI27sASDD4zo3kmj2ZkqqPWMZo3iRVEGAECBxHIt8TWIY5GZus4zCcDSoFjoG69LqkWF3ceZkc-6EWDUNI3a7oHy8jw8-6wWbV9bgKaxwhWvJOwuuSeoUvdDQ6OylOess0hIevxGf_kPheOT66q-2aMQ-HQwQJHKbZAaLy75TgnX9xRWv9_TPnPKFkFsOZIDeyX9IelaDp9K_nVNUx44E-02xgM5Vtfi9X5Ciyt7NIi9YjEJJtI9mkW6puForsdEwIu3P5cfiRejQc8cx_1urbkgh2K1ifayX5F8tkpD_yjjm2-1f2GR6Qxp2HQfW2UT33powQNIww-cpclypaNFrqoF3owlm0DhtEH7iLHmwqt7RHRsHSG9D7Fw-6OaQm-fHnTrTWMhLae2Xan967OtqaVsowjjAAm3bprHKHdbp-li-vRjHdMUeWDGWj5pCUJALty-KP43ZkG-cjDVvv8dODVVrQJ0hZepdvuMJIRhId_boVx-Jr4Sq2QbRpcEPxZSCMohIoejzIX5hmETI-HiuAlKPiwPdq_MnFVptUKFsNzzyW508fGEjsRr9W_DIqv7Zra2eHPYErauyAMIZpU6zd9JGRz1CC668BiDTLLMaoRv6SlN49n6FW3N3lN01Q2WyR45nike52DifvoDZFyHYOg-iKhYVFiVZo8nU8G8GZI=w398-h266-no",
+        Caption: "SMC Supper"
+      }
+    ],
+    FH: [
+      {
+        Image:
+          "https://lh3.googleusercontent.com/AgWH1uTqSJuWxFI7i8qG3GpVZ3BeJ_HaEkFmMXg45da7k_aLwjleNE6tZwjTlQyfYLT5SpKdn3RgkfOu08hMdqacnXa74SbboFHUWxGOiEUsw3Th1uIp442YaDzAnvJFhbSLbl0z6KyT5wRBFIXC3F7CKy0Lc_2yT5g5vHZ--VKwy5adaxrtC_IuWnMmJHWK0LO3siLp2_nrAfK52ANOE5hSPiVacO73fedX4srgxOoLcBuAs6aO4RD5rvUIByvT5sGKxHXQQtwORQKBKrzhpF4mnpnUb-HfPxeozEEHRMxKGFMAO0Iem-UoZj-EYKWvscxiT7c3ITEWBWilDH37TGT5b-XVbDCBy8NBH2_Z23s3_RAktlZkN8mgcRTOvo3iAzInGKyMTOIbiOf1_JZ3J1tVzYXlIZ8zkUs2WvLwrgD7GbhjbypL25Lhg8S9X2ICOthIz838upvKu1jFZVv4zZg7rs4ZiRe5hZFBevHY_83DTHWZyGni2B8wTEFF5OWugI6YIRTppUexM5HT9ZHnc0RiGcqGy6CY_SDFB-kvsD0J4MNhGg7wk5IPHMwiUa4HEgdZ_DKrr0gHvLwhU_erVPvcHm5krVlseI96CJt89VcoZhfDk5N5G_5OS2mcEq0-LBjSSueqtR69Vw2S9N6DD5jNmTtgJdaaFApjtSHN7ogltkxGtmefb1w=w1948-h1298-no",
+        Caption: "Halloween"
+      },
+      {
+        Image:
+          "https://lh3.googleusercontent.com/fs81k8Y9SaHaB4KUjQsAvjjgPp5I63PxXi2pYc4L7fr7raU7QcdWlkuLzZQTC1qVcrE1q_ja4O3e2RWLJfc2hIA4PBDm-o6usKCbEFGNjG8S4fwp8H9fGYArAKfNz8fa_4Fb8sGwhQXMtF8lDKGDKvP9GSbjB636d4jpz950xOSTBks4tGCe4ZTwKdtje5VFd-dVRmmSqDbz1dzjxAXXpCq1SVrXZFB4zPL1HCNM3g_WCW3jV8lTnLmTN7LaluIvKhbsDP-mGObkCJ0rr8kLOk4xNkbxSPTIeGaXe2Qy2dMbx1jKxrmRVXf01rEVsfUkZrpjk754n_hqYmvWeD_tZDb78Pp84TA4uE9q7MxmWll4fhHUVeEZyp9IXmAmmWBVwj-cXSs_kuxLcqVdWEkJVGnH7kXcJ5psav5wI4POwGiZgMUMlGPX6bnE8Jl6H-IHfqAwF-R8zLhGQO5tX8fYI0m8-acdOyuWSJdn4CGm9hUhnMl5BdR7ePWxrBSWbgBbsiVSwecmti4M89eVTSpYmexYt-EWodmmWrkv317tPwic6hxZwpdwS6ArG23t8i2juRw8OS-J3_CfvzXzDxyi3fM9PKv_RYwt-DdBVuAzOneEUqxd4oZ90QeY4WZ4iSrsabGSOGsLcsFyOhIQPbS9Sgk_DCVx5Ygl6-WDsBhGva6egTyUWQI99v8=w1948-h1298-no",
+        Caption: "Culture Night"
+      },
+      {
+        Image:
+          "https://lh3.googleusercontent.com/VpHKjJf4_BHZb435Csb4o2RWg7pslZ4YuYMHoRtnzS3k4XTFGGrRnFC7E8cicPfGauYiqO3A3Pwjmpb0W5hJivxtU9nVvdRt5BIViO524YoacUG-Ap9Z-UdRcgmTalNPBadWNWSqK9Uygw2MvTDSrt1rvcJ6yOKzf9QXUeKHsQQqaaQ_JoyzOZVDHfAVBLS41monGpCU74s9CN7FiH45v7L__VVlkQL1Y4kDcoKUzTs-eOjXuwnQoXH5THj0PPGHcjYr-zMdgQwNZovgoTFF8yr5vL119wa29BuLpHzAy5e7jv2Cs7pXd4Dcx4ud59KHqg9vg6PaNEHRkq7kv85dz-owH3tIi_fPke_oO3kHS3SvqinGXGsllv6Qr1o_BvShKZrqCujE7VETQYwWppI3EZdxIbQguSWFvKB3BMkFWzeC76uq2TF3sYyQoZNGkmr5JTBX4vAN0VRedPhv1Navw02ycEbYBhyo04p6yNY6aaicWzoea0nh-U-vUbRU_aeIZf43J9n-vIDQ3IKPvRQgnuuGuLRtAnE_Jy9CtMdAM7cRrlt_pHG-Fu4vWywDLyUtQjQKeUuZIjm0Fq24vUBB65LtQvQmTKJUBmc87maGps3YJliobJjNz39E33hCJs3G4NG_8WoMoaAgHvkwl9u6iJKqQQW-2S7kEGdwXzgbkSAiHhjuwoV24OA=w287-h191-no",
+        Caption: "La Soriee"
+      },
+      {
+        Image:
+          "https://lh3.googleusercontent.com/ZPc91Z9-CcnZx895_KMWnriNEeBflc1xtPF9nwFoucP1SmI18QGjkZLVWYPP3LJ91YqNAqNUO3xL4tRExYF8iG6LcqSqR4Iqw6j9m4EPWQqSY4EIpAkoAhQvNe6UXhlky9dwBNSYCQRU62_pD_sg4XEqAzsVUVJc0b4IombT_bBMfRAWXLzMIL4OADQ07gIIHC0w8fMboW7EiYjnp0pFzxHx7oPVFqcmyA0pgC09FE8cvHR-h1kDeK-9bJr9xn1cJNivJx14xsNR2rb3MjmY3Ux5I09k1yE3lvn0xbAOYqq0yqTBCy2cFmgHX-z7rlkBmAvdjbk5Hc6VGTuQPtKoBpRvSA2qV_5FY0cCc9yhhF5GthwIJnrRgKhsY25g__noM0f0IvmN5h5Qx8tFI0mg9qjX7RBU1YKC7YVtTg-KQ_vHoz4tt7XZ4xqlKP1PWju4FpoYKFue79xc6Saz_IYmIM2LFZLLgQY7U2LAXKIXTZsHvLqizjBlTH5HuKnKAk5j0ryyY2zFIeD33lD2hwucJfdsKwAXL02JpwXOfFNKyJw8DHsg97RJn4nds2KOOoPoGxYkh73bFg-1tiAccDic87EuOmRKx_xJtlcGC2skXW-1Hgw6HcP8ti-hZJbyNwEKZs1rr3JZrjgPz4LXA2QiajJTxeUpB7Dg4iU2rV2xTofVMB0KRbvd-b0=w1948-h1298-no",
+        Caption: "EHOC"
+      }
+    ]
+  },
+
+  entrance:
+    "https://lh3.googleusercontent.com/0bxOqngwh3UGq5j_1MYFSAoMryBHWthYimnSn9t8NvykLrHo2E0bWXYVtPySzXYt9GCuxGnV8w4l9zQbwzYvGlvWJh749ALjAyPoOOhxFyk0UGjaSn3A0T-k-r6E7UpOVncTFpC4-4ppMvCwrfXewg6YdVW5OqH4Rbd3r44VHgDzfhF0xzD0aC3FjRxFNHOswmvcwMmUxx5mdHHeZEJq2pTkybtqPfWcCKJVHnaRZEjBewdtME_P3SwpDKuA82YldfRu0lfh4nOYm2NGIYCAU5Kodg8VWp5CB6W-CkGLsBNxe0nGapCn3jGx0jnPgXJ8L34yxMZ60m_oSMtOtaswIjQ3eEUk8hDGmhgnNSgwquEktszAH554n_XT6YQ5zz5acuoBP-HRy0h-MNyFiXYtxudcbfj8Ug5KFYEHaX7InzL1DWEBbNo4cp5R0DH1h-DUBWojzA2eeZOokjqKZ_zl9djo5xv_MOcubRd_lxnYoe1S8sYoFlcACgCgTcVcRDiHWv3Goc9c9Ai0TyMf1-UbYzLBUbHSyRMOyeR-lfFMGBlCRc8DUnWzs8K0TK_5LdY5_DPLpBIGLFX-Z4mEPtmJ1gvODkrpT4bBTxEMG9KjzCbfhnAHO4nGPPq93K_i4tqwignEf_le0LSp82weaYL_nX8eztSjI13Am5QZI6Xrcx8bZ0Q3JKF6Buo=w1440-h810-no",
+
+    transitiongifs: {
+      ABBAS:ABBAS,
+      ABBB1:ABBB1,
+      ABBBL:ABBBL,
+      APANAS:APANAS,
+      ASABB:ASABB,
+      ASAPAN:ASAPAN,
+      ASMPC:ASMPC,
+      B1ABB:B1ABB,
+      B1GGS:B1GGS,
+      B1L1C:B1L1C,
+      BLABB:BLABB,
+      DHDHS:DHDHS,
+      DHSDH:DHSDH,
+      DHSFHS:DHSFHS,
+      DHSRRS:DHSRRS,
+      DSDSS:DSDSS,
+      DSSDS:DSSDS,
+      DSSGYM:DSSGYM,
+      DSSL1S:DSSL1S,
+      FHFHS:FHFHS,
+      FHSDHS:FHSDHS,
+      FHSFH:FHSFH,
+      FHSL1S:FHSL1S,
+      GGSB1:GGSB1,
+      GGSHO:GGSHO,
+      GYMDSS:GYMDSS,
+      HOGGS:HOGGS,
+      HORRS:HORRS,
+      L1CB1:L1CB1,
+      L1CL1S:L1CL1S,
+      L1SDSS:L1SDSS,
+      L1SFHS:L1SFHS,
+      L1SL1C:L1SL1C,
+      MPCAS:MPCAS,
+      RRRRS:RRRRS,
+      RRSDHS:RRSDHS,
+      RRSHO:RRSHO,
+      RRSRR:RRSRR
+    }
+};
